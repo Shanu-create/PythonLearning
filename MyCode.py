8b1f9a0974d22a91d955a97fc7e1f9afd35c6f6e
Shanu-create <76220168+Shanu-create@users.noreply.github.com>
2023-01-26 09:54:15 +0100

    Add files via upload

---
+++ MyCode.py
@@ -0,0 +1,41 @@
+# Addition
+print(2 + 3)
+
+# Subtraction
+print(9 - 4)
+
+# Multiplication
+print(5 * 8)
+
+# Division
+print(4 / 2)
+# returns the value interms of a float value
+print(5 // 3)
+# returns only the quotient
+print(5 % 3)
+# returns only the remainder
+
+# combination of arithmetric operations
+print(8 + 5 - 9)
+
+# cube of 2
+print(2 ** 3)
+
+# printing strings
+print('shanu')
+print("sagari")
+#print('shanu's laptop')  #invalid syntax
+print("shanu's laptop")
+print('shanu "laptop"')
+#print('shanu's "laptop"') #invalid syntax
+print('shanu\'s "laptop"') #forward slash ignores the special meaning of single quote
+
+#to print a text ,n number od times
+print(10 * 'sri rama jayam\n')
+
+#to print a path
+print('c:\docs\new')
+#gives output as
+#c:\docs
+#ew
+print('c:\docs\\new')
